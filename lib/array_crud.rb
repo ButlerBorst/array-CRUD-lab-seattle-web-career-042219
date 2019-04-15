36e9f5322df4d35dd784ff28f8a2eec790579949
Alex Borst <butler.borst@gmail.com>
2019-04-15 15:33:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   full_array = ["a", "b", "c", "d"]
-  full_array << "e"
+  full_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
