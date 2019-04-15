e26b7812b5f82e010a3ae3d911ec8b2691d2e11d
Alex Borst <butler.borst@gmail.com>
2019-04-15 15:36:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  full_array = ["a", "b", "c", "d"]
-  full_array {[]
+  full_array = ["a", "am", "am", "d"]
+  full_array [2]
 end
 
 def retrieve_first_element_from_array(array)
