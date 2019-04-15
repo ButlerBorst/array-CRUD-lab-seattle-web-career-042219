3bf4269f53115432796d9dec08921b75d381676d
Alex Borst <butler.borst@gmail.com>
2019-04-15 15:34:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   full_array = ["a", "b", "c", "d"]
-  full_array.unshift "e"
+  full_array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
