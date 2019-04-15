61407c74d477ddd7b83b8f9390af89b50dd3e135
Alex Borst <butler.borst@gmail.com>
2019-04-15 15:37:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  full_array = ["a", "b", "c", "d"]
+  full_array = ["a", "b", "c", "arrays!"]
   full_array.pop "arrays!"
 end
 
